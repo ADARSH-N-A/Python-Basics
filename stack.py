ae20ec3ced2cc3a051f7b290cd574893ce3af1c6
ADARSH-N-A <adarshna2005@gmail.com>
2026-06-09 18:02:42 +0530

    Add files via upload

---
+++ stack.py
@@ -0,0 +1,6 @@
+from collections import deque
+st = deque()
+st.append(10)
+st.append(20)
+st.popleft()
+print(st)
---
+++ valid_path.py
@@ -0,0 +1,5 @@
+s = "   fly me   to   the moon  "
+print(s)
+s1 = s.split(" ")
+word = s1[-1]
+print(len(word))
